c155a100fa69ef76cff32ef7a336ec09baa3aa53
Icaro <119881183+Ya-502@users.noreply.github.com>
2024-04-08 16:52:33 -0300

    criando o app android

---
+++ BancoDIP/app/src/main/java/com/example/bancodip/view/RegisterActivity.java
@@ -0,0 +1,16 @@
+package com.example.bancodip.view;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.bancodip.R;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+    }
+}
