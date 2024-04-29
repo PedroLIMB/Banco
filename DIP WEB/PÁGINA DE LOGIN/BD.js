bdc5e3622c1cc0e5191d65131dabb493ce114b9f
Diogo Vieira Braz <diovbraz@gmail.com>
2024-04-29 15:55:50 -0300

    DIP WEB Atualizado

--- DIP WEB/PÁGINA DE LOGIN/BD.js
+++ DIP WEB/PÁGINA DE LOGIN/BD.js
@@ -25,7 +25,9 @@ function cadastrar(event) {
   };
 
   localStorage.setItem(email, JSON.stringify(usuario));
+  localStorage.setItem('emailLogado', email); // Adiciona o email do usuário logado ao localStorage
   alert('Usuário cadastrado com sucesso');
+  window.location.href = '/BANCO/index.html'; // Redireciona para a página do banco
 }
 
 function login(event) {
@@ -38,8 +40,9 @@ function login(event) {
   if (userData) {
     const usuario = JSON.parse(userData);
     if (senha === usuario.senha) {
+      localStorage.setItem('emailLogado', email); // Adiciona o email do usuário logado ao localStorage
       alert('Login realizado com sucesso');
-      window.location.href = '/BANCO/index.html';
+      window.location.href = '/BANCO/index.html'; // Redireciona para a página do banco
     } else {
       alert('Senha incorreta');
     }
